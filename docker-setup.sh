0e076b6b91c7229e982ca7829631af1c4e1582f2
Koliy82 <sokol070804@mail.ru>
2024-10-09 00:42:10 +0300

    chore: add some hints

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,8 +2,11 @@
 
 echo "Building Docker container..."
 docker compose build
-
 read -p "Please enter your Steam Guard code: " STEAM_GUARD_CODE
 
 echo "Running the application with Steam Guard code..."
-docker compose run steam-app node index.js "$STEAM_GUARD_CODE"
+echo "To exit the log console, press CTRL+C"
+echo " "
+
+
+docker compose run steam-app node index.js "$STEAM_GUARD_CODE" -d
