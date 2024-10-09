a91d92924ad6a4972add236f9aafa353b646e215
Koliy82 <sokol070804@mail.ru>
2024-10-09 13:01:31 +0300

    feat: save jwt session after input guard code + load with check expired

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
-
 const SteamUser = require('steam-user');
+const ReadLine = require('readline');
+const fs = require('fs')
+const path = require('node:path'); 
 
 const user = new SteamUser();
 
 const logOnOptions = {
   accountName: process.env.STEAM_USERNAME,
   password: process.env.STEAM_PASSWORD,
-  twoFactorCode: process.argv[2],
 };
 
-user.logOn(logOnOptions);
+token_path = logOnOptions.accountName + ".secret";
+
+if (fs.existsSync(user.storage.directory + path.sep + token_path)) { 
+  user.storage.readFile(token_path).then( bytes => {
+    token = bytes.toString()
+    console.log('token ' + token_path + ' is loaded:');
+    jwt = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+    console.log(jwt);
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (jwt.exp && currentTime >= jwt.exp) {
+      console.log('Token has expired. Logging in with username and password...');
+      user.logOn(logOnOptions);
+    } else {
+      console.log('Token is still valid.');
+      user.logOn({ refreshToken: token });
+    }
+  });
+}else{
+  user.logOn(logOnOptions);
+}
 
 user.on('loggedOn', () => {
   console.log(logOnOptions.accountName + ' - Successfully logged on');
@@ -24,8 +44,20 @@ user.on('loggedOn', () => {
   }
   user.gamesPlayed(games); 
   console.log('Playing games:', games);
-  
 });
 
+user.on('steamGuard', function(domain, callback) {
+  let rl = ReadLine.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+  rl.question('Steam Guard code: ', (code) => {
+			rl.close();
+      callback(code);
+	});
+});
 
-
+user.on('refreshToken', function(token) {
+	user.storage.saveFile(token_path, token)
+  console.log(token);
+});
