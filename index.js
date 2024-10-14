aedb8f8592f46f17166c3c8657f5d59e7af2abc3
Koliy82 <sokol070804@mail.ru>
2024-10-15 02:42:19 +0300

    feat: subscribe on user events

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const i18next = require('i18next');
 const Backend = require('i18next-node-fs-backend');
 require('dotenv').config();
+const SteamUser = require('steam-user');
+const totp = require('steam-totp');
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 const mongoUrl = process.env.MONGO_URL;
@@ -16,6 +18,7 @@ const client = new MongoClient(mongoUrl,  {
 });
 let db = client.db(mongoDB);
 db.command({ ping: 1 })
+const usersColl = db.collection('users');
 console.log("Connected to MongoDB");
 
 i18next.use(Backend).init({
@@ -28,13 +31,89 @@ i18next.use(Backend).init({
 
 async function findOrCreateUser(telegramId, callback) {
   const usersCollection = db.collection('users');
-  user = await usersCollection.findOne({ id: telegramId });
+  const user = await usersCollection.findOne({ id: telegramId });
   if (!user) {
-    result = await usersCollection.insertOne({ id: telegramId, accounts: [] });
+    const result = await usersCollection.insertOne({ id: telegramId, accounts: [] });
     callback(result.ops[0]);
   } else {
     callback(user);
   }
+}
+
+function subscribeUserEvents(user, account) {
+  user.on('loggedOn', () => {
+    console.log(`${account.login} - Successfully logged on`);
+    user.setPersona(account.state);
+
+    let games = account.gameIds;
+
+    if (Array.isArray(games) && games.every(game => typeof game === 'string' && /^\d+$/.test(game))) {
+      games = games.map(Number);
+    }
+
+    user.gamesPlayed(games);
+    console.log('Playing games:', games);
+  });
+
+  user.on('steamGuard', (domain, callback) => {
+    bot.sendMessage(account.telegramId, 'Введите код Steam Guard.');
+    bot.once('message', (codeResponse) => {
+      const code = codeResponse.text;
+      callback(code);
+    });
+  });
+
+  user.on('refreshToken', async function(token) {
+    await usersColl.updateOne(
+      { id: account.telegramId, "accounts.login": account.login },
+      { $set: { "accounts.$.token": token } }
+    );
+    console.log(`Auth token for ${account.login} has been saved.`);
+  });
+
+  user.on('playingState', (blocked, playingApp) => {
+    if (blocked) {
+      console.log(`${account.login} is playing on another device: ${playingApp}`);
+      bot.sendMessage(account.telegramId, `Вы играете в другую игру на ${playingApp}, фарм остановлен.`, {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Продолжить фарм', callback_data: `continue_farming_${account.login}` }]
+          ]
+        }
+      });
+      user.logOff();
+    }
+  });
+
+  user.on('playingState', (blocked, playingApp) => {
+    if (blocked) {
+      console.log(`${account.login} is playing on another device: ${playingApp}`);
+      bot.sendMessage(account.telegramId, `Вы играете в другую игру на ${playingApp}, фарм остановлен.`);
+    }
+  });
+
+  user.on('error', (err) => {
+    console.error(`${account.login} encountered an error:`, err);
+    bot.sendMessage(account.telegramId, `Ошибка у ${account.login}: ${err.message}`);
+    user.logOff();
+  });
+}
+
+function addOrUpdateAccount(userId, account, sharedSecret = null) {
+  const accountsCollection = db.collection('users');
+  const accountData = {
+    login: account.login,
+    password: account.password,
+    shared_secret: sharedSecret
+  };
+
+  accountsCollection.updateOne(
+    { id: userId },
+    {
+      $push: { accounts: accountData }
+    },
+    { upsert: true }
+  );
 }
 
 bot.onText(/\/start/, (msg) => {
@@ -48,8 +127,6 @@ bot.onText(/\/add (.+) (.+)/, (msg, match) => {
   const chatId = msg.from.id;
   const login = match[1];
   const password = match[2];
-  console.log(login);
-  console.log(password);
   findOrCreateUser(chatId, (user) => {
     console.log(user);
     i18next.changeLanguage(msg.from.language_code || 'en').then(() => {
@@ -64,17 +141,27 @@ bot.onText(/\/add (.+) (.+)/, (msg, match) => {
         }
       });
     });
-
-
     bot.once('callback_query', (callbackQuery) => {
       const response = callbackQuery.data;
       if (response === 'yes') {
         bot.sendMessage(chatId, 'Отправьте свой shared secret.');
         bot.answerCallbackQuery(callbackQuery.id)
         bot.once('message', (secretResponse) => {
-          const sharedSecret = secretResponse.text;
-          saveAccountToDB(chatId, login, password, sharedSecret);
-          bot.sendMessage(chatId, 'Аккаунт добавлен!');
+          const new_account = {
+              login: login,
+              password: password,
+              shared_secret: secretResponse.text
+          }
+          const user = new SteamUser();
+          subscribeUserEvents(user);
+          addOrUpdateAccount(new_account);
+          user.logOn({
+            accountName: account.login,
+            password: account.password,
+            machineName: "Koliy82",
+            clientOS: 20,
+            twoFactorCode: totp.generateAuthCode(sharedSecret)
+          });
         });
       } else if (response === 'no') {
         bot.sendMessage(chatId, 'Отправьте код Steam Guard.');
@@ -84,33 +171,14 @@ bot.onText(/\/add (.+) (.+)/, (msg, match) => {
           saveAccountToDB(chatId, login, password);
           bot.sendMessage(chatId, 'Аккаунт добавлен!');
         });
-        
       }
     });
   });
 });
 
-function saveAccountToDB(userId, login, password, sharedSecret = null) {
-  const usersCollection = db.collection('users');
-  usersCollection.updateOne(
-    { id: userId },
-    {
-      $push: {
-        accounts: {
-          login,
-          password,
-          shared_secret: sharedSecret,
-          JWT: {}
-        }
-      }
-    },
-    { upsert: true }
-  );
-}
-
 bot.onText(/\/list/, async (msg) => {
   const chatId = msg.chat.id;
-  const user = await db.collection('users').findOne({id: chatId});
+  const user = await usersColl.findOne({ id: chatId });
 
   if (user && user.accounts.length > 0) {
     let message = 'Ваши аккаунты:\n';
@@ -123,7 +191,41 @@ bot.onText(/\/list/, async (msg) => {
   }
 });
 
-// require('dotenv').config();
+bot.onText(/\/donate/, (msg) => {
+  i18next.changeLanguage(msg.from.language_code || 'en').then(() => {
+    const donateText = i18next.t('donate');
+    bot.sendMessage(msg.chat.id, donateText);
+  });
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  const message = callbackQuery.message;
+  const callbackData = callbackQuery.data;
+  
+  if (callbackData.startsWith('continue_farming_')) {
+    const login = callbackData.split('continue_farming_')[1];
+    
+    usersColl.findOne({ id: message.chat.id, "accounts.login": login }).then(user => {
+      if (user) {
+        const account = user.accounts.find(acc => acc.login === login);
+        
+        // Повторная авторизация
+        const steamUser = new SteamUser();
+        steamUser.logOn({
+          accountName: account.login,
+          password: account.password,
+          refreshToken: account.token,  // Используем обновленный токен
+          machineName: "Koliy82",
+          clientOS: 20,
+        });
+
+        subscribeUserEvents(steamUser, account);
+        bot.sendMessage(message.chat.id, `Фарм на аккаунте ${login} возобновлен.`);
+      }
+    });
+  }
+});
+
 // const SteamUser = require('steam-user');
 // const fs = require('fs');
 // const path = require('path');
@@ -132,36 +234,36 @@ bot.onText(/\/list/, async (msg) => {
 // function logIntoAccount(account) {
 //   const user = new SteamUser();
 
-//   const logOnOptions = {
-//     accountName: account.login,
-//     password: account.password,
-//     machineName: "Koliy82",
-//     clientOS: 20,
-//   };
+  // const logOnOptions = {
+  //   accountName: account.login,
+  //   password: account.password,
+  //   machineName: "Koliy82",
+  //   clientOS: 20,
+  // };
 
-//   const tokenPath = `${logOnOptions.accountName}.secret`
-//   if (fs.existsSync(user.storage.directory + path.sep + tokenPath)) {
-//     user.storage.readFile(tokenPath).then(bytes => {
-//       const token = bytes.toString();
-//       console.log('Token ' + tokenPath + ' is loaded:');
-//       const jwt = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-//       const currentTime = Math.floor(Date.now() / 1000);
+  // const tokenPath = `${logOnOptions.accountName}.secret`
+  // if (fs.existsSync(user.storage.directory + path.sep + tokenPath)) {
+  //   user.storage.readFile(tokenPath).then(bytes => {
+  //     const token = bytes.toString();
+  //     console.log('Token ' + tokenPath + ' is loaded:');
+  //     const jwt = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+  //     const currentTime = Math.floor(Date.now() / 1000);
 
-//       if (jwt.exp && currentTime >= jwt.exp) {
-//         console.log(`Token ${account.login} has expired. Logging in with username and password...`);
-//         user.logOn(logOnOptions);
-//       } else {
-//         console.log(`Token ${account.login} is still valid.`);
-//         user.logOn({
-//           refreshToken: token,
-//           machineName: "Koliy82",
-//           clientOS: 20,
-//          });
-//       }
-//     });
-//   } else {
-//     user.logOn(logOnOptions);
-//   }
+  //     if (jwt.exp && currentTime >= jwt.exp) {
+  //       console.log(`Token ${account.login} has expired. Logging in with username and password...`);
+  //       user.logOn(logOnOptions);
+  //     } else {
+  //       console.log(`Token ${account.login} is still valid.`);
+  //       user.logOn({
+  //         refreshToken: token,
+  //         machineName: "Koliy82",
+  //         clientOS: 20,
+  //        });
+  //     }
+  //   });
+  // } else {
+  //   user.logOn(logOnOptions);
+  // }
 
 //   user.on('loggedOn', () => {
 //     console.log(logOnOptions.accountName + ' - Successfully logged on');
